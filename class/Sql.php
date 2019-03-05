3dcb76ddfb45fedd36798ef99d23119e1370ec56
Eliezer A Mottin <eliezer.apple.mottin@gmail.com>
2019-03-05 10:39:07 -0300

    criamos a classe usuario

--- class/Sql.php
+++ class/Sql.php
@@ -9,9 +9,9 @@ class Sql extends PDO{
 
 
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 		}
 	}
 
