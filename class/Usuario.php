f1b7b3c9fdd4cd5324627d1a337e91b78de5a32b
Eliezer A Mottin <eliezer.apple.mottin@gmail.com>
2019-03-05 12:08:11 -0300

    Quse tudo os comandos

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,13 +1,13 @@
 <?php 
 
 class Usuario {
-
+	//atributos que compoe a classe é a mesma da tabelas
 	private $idusuario;
 	private $deslogin;
 	private $dessenha;
 	private $dtcadastro;
 
-
+	//metodos Gets e Setters
 	public function getIdusuario(){
 		return $this->idusuario;
 	}
@@ -41,6 +41,7 @@ class Usuario {
 	}
 
 
+	//Essa funcao traz todas as informacoes da usuario, mas apenas daquele ID que foi passado por parametro
 	public function loadById($id){
 		$sql = new Sql();
 
@@ -59,6 +60,52 @@ class Usuario {
 		}
 	}
 
+	//Essa funcao traz uma lista de todos os usuarios da tabelas 
+	public static function getList(){
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+	}
+
+
+	//Essa funcao faz a busca com base no login que é digitado
+	public static function search($login){
+		
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
+
+	public function login($login, $password){
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			':LOGIN'=> $login,
+			':PASSWORD'=>$password
+		));
+
+		//if(isset($results()) posso fazer desse jeito ou desse jeito (abaixo)
+		if(count($results) > 0){
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}else{
+
+			throw new Exception("Login e/ou senha inválidos");
+			
+
+		}
+	}
+
+
+	//Funcao Magica, mostras as informacoes da tabelaa ususarios, quando a classe for instanciada
 	public function __toString(){
 		return json_encode(array(
 
--- index.php
+++ index.php
@@ -4,20 +4,31 @@
 require_once("config.php");
 /*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios) ;
 */
 
+
+/*
+*carrega um usuário
 $root = new Usuario();
-
 $root->loadById(3);
+echo $root;*/
 
-echo $root;
+//carrega uma lista de usuário
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
 
+//carrega uma lista de ussuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","!@#$");
+
+echo $usuario;
 
 
  ?>
